59248c56101680146a627272ec1c50816cce791c
James Sapp <jameseisapp@gmail.com>
2021-01-20 09:55:24 -0600

    commit tree 7eb696cc8577cbed9d161c49e2fdbbbbcbeb156d

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+
+
 	def index
 		@posts = Post.all
 	end
@@ -7,6 +9,8 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+
+
 	def new
 		@post = Post.new
 		@categories = Category.all
@@ -14,8 +18,11 @@ class PostsController < ApplicationController
 
 	def create
 		post = Post.create(post_params)
+
 		redirect_to post_path(post)
 	end
+
+
 
 	def edit
 		@post = Post.find(params[:id])
@@ -25,12 +32,23 @@ class PostsController < ApplicationController
 	def update
 	  post = Post.find(params[:id])
 	  post.update(post_params)
+
 	  redirect_to post_path(post)
 	end
+
+	def destroy 
+		# @post = Post.find(params[:id])
+		# @post.destroy 
+		byebug
+		redirect_to posts_path(@)
+	end
+
+
 
   private
 
   def post_params
     params.require(:post).permit(:title, :description)
   end
+
 end
